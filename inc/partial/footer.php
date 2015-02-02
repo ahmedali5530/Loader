a86b3195f2607cf404191581d014250421f60268
Ahmed Ali <ahmedali5530@gmail.com>
2015-02-02 17:51:53 +0500

    Create footer.php

---
+++ inc/partial/footer.php
@@ -0,0 +1,20 @@
+</div>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+            	<button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="fa fa-times fa-lg"></i></button>
+            	<h3 class="modal-title" id="modal-title"></h3>
+            </div>
+            <div class="modal-body" id="modal">
+            	<p></p>
+            </div>
+            <div class="modal-footer">
+            	<button type="button" class="btn btn-success" data-dismiss="modal">Save Changes <i class="fa fa-check"></i></button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel <i class="fa fa-times"></i></button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+</body>
+</html>
