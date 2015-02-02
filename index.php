d4148cf38fec1ecafcdb2c27f6231dfd0705eff3
Ahmed Ali <ahmedali5530@gmail.com>
2015-02-02 17:47:43 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php require_once('config.php');?>
+<?php
+	echo $loader->loader->load('inc/partial/header');
+	
+?>
+
+<div class="col-md-48 col-sm-48 col-xs-48 main bg-white">
+	<div class="getayats"></div>
+	<div class="bismillah"></div>
+</div>
+
+<script type="text/javascript">
+
+</script>
+<?php echo $loader->loader->load('inc/partial/footer');?>
